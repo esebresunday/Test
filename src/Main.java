519e6e7f1783fa4ed125cf0053c5f6ad736e1532
Sunday Esebre <esebresunday321@gmail.com>
2024-11-25 13:31:51 -0600

    Test message

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        Cat c = new Cat();
+        c.m();
+    }
+}
+class Cat
+{
+    public void m()
+    {
+        System.out.println("This is a Cat class");
+    }
+}
